7d7a8b4960e48244b0a917d2213477f9371aca45
venfran <fgvr92@gmail.com>
2024-07-27 13:01:39 -0300

    fix: add function to add product to cart

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,9 +1,5 @@
 import { createClient } from '@/utils/supabase/server';
-import {
-  Cart,
-  CartProduct,
-  CartProductWithProduct,
-} from '@/utils/types/cart.interface';
+import { CartProductWithProduct } from '@/utils/types/cart.interface';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET() {
@@ -22,79 +18,104 @@ export async function GET() {
   });
 }
 
+// export async function POST(request: NextRequest) {
+//   const supabase = createClient();
+//   const { profile_id, design_id, fabric_id } = await request.json();
+
+//   const { data: cart, error: cartError } = await supabase
+//     .from('cart')
+//     .select('*')
+//     .returns<Cart[]>();
+
+//   if (cartError) {
+//     return NextResponse.json({
+//       data: cart,
+//       error: cartError,
+//     });
+//   }
+
+//   if (cart.length === 0) {
+//     const { data: cartData, error: cartError } = await supabase
+//       .from('cart')
+//       .insert({})
+//       .select('*')
+//       .returns<Cart[]>();
+//     if (cartError) {
+//       return NextResponse.json({
+//         data: cartData,
+//         error: cartError,
+//       });
+//     }
+
+//     const { data: cartProductData, error: cartProductError } = await supabase
+//       .from('cart_product')
+//       .insert({
+//         cart_id: cartData[0].id,
+//         profile_id,
+//         fabric_id,
+//         design_id,
+//       })
+//       .select('*')
+//       .returns<CartProduct[]>();
+
+//     if (cartProductError) {
+//       return NextResponse.json({
+//         data: cartProductData,
+//         error: cartProductError,
+//       });
+//     }
+
+//     return NextResponse.json({
+//       data: cartProductData,
+//       error: cartProductError,
+//     });
+//   }
+
+//   const { data: cartProductData, error: cartProductError } = await supabase
+//     .from('cart_product')
+//     .insert({
+//       profile_id,
+//       fabric_id,
+//       design_id,
+//       cart_id: cart[0].id,
+//     })
+//     .select('*')
+//     .returns<CartProduct[]>();
+
+//   if (cartProductError) {
+//     return NextResponse.json({
+//       data: cartProductData,
+//       error: cartProductError,
+//     });
+//   }
+
+//   return NextResponse.json({
+//     data: cartProductData,
+//     error: cartProductError,
+//   });
+// }
+
 export async function POST(request: NextRequest) {
-  const supabase = createClient();
-  const { profile_id, design_id, fabric_id } = await request.json();
-
-  const { data: cart, error: cartError } = await supabase
-    .from('cart')
-    .select('*')
-    .returns<Cart[]>();
-
-  if (cartError) {
-    return NextResponse.json({
-      data: cart,
-      error: cartError,
-    });
-  }
-
-  if (cart.length === 0) {
-    const { data: cartData, error: cartError } = await supabase
-      .from('cart')
-      .insert({})
-      .select('*')
-      .returns<Cart[]>();
-    if (cartError) {
-      return NextResponse.json({
-        data: cartData,
-        error: cartError,
-      });
-    }
-
-    const { data: cartProductData, error: cartProductError } = await supabase
-      .from('cart_product')
-      .insert({
-        cart_id: cartData[0].id,
-        profile_id,
-        fabric_id,
-        design_id,
-      })
-      .select('*')
-      .returns<CartProduct[]>();
-
-    if (cartProductError) {
-      return NextResponse.json({
-        data: cartProductData,
-        error: cartProductError,
-      });
-    }
-
-    return NextResponse.json({
-      data: cartProductData,
-      error: cartProductError,
-    });
-  }
-
-  const { data: cartProductData, error: cartProductError } = await supabase
-    .from('cart_product')
-    .insert({
+  try {
+    const supabase = createClient();
+    const { profile_id, design_id, fabric_id } = await request.json();
+    const { data, error } = await supabase.rpc('add_product_to_cart', {
       profile_id,
-      fabric_id,
       design_id,
-      cart_id: cart[0].id,
-    })
-    .select('*')
-    .returns<CartProduct[]>();
-
-  if (cartProductError) {
-    return NextResponse.json({
-      data: cartProductData,
-      error: cartProductError,
+      fabric_id,
     });
-  }
 
-  return NextResponse.json({
-    data: cartProductData,
-    error: cartProductError,
-  });
+    return NextResponse.json({
+      data,
+      error,
+    });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        error: error.message,
+        data: null,
+      },
+      { status: 500 }
+    );
+  }
 }
